5e46e8c1ebaa86e3ceabdae4c70bc99346ed0442
AndrezaCarrilho <108102040+AndrezaCarrilho@users.noreply.github.com>
2025-06-15 19:35:25 -0300

    Correção forms.py

--- core/forms.py
+++ core/forms.py
@@ -1,7 +1,72 @@
-from django.apps import AppConfig
+from django import forms
+from .models import Visitor, Reservation, Communication, Finance, Vehicle, Apartment, Orders
 
-class CoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
-    def ready(self):
-        pass
+
+# Definindo o formulário de visitante
+class CommunicationForm(forms.ModelForm):
+    class Meta:
+        model = Communication
+        fields = ['recipients', 'subject', 'message']
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user', None)
+        super().__init__(*args, **kwargs)
+
+# Definindo o formulário de reserva
+class ReservationForm(forms.ModelForm):
+    class Meta:
+        model = Reservation
+        fields = ['space', 'start_time', 'end_time']
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user', None)
+        super().__init__(*args, **kwargs)
+
+
+class ApartmentForm(forms.ModelForm):
+    class Meta:
+        model = Apartment
+        fields = ['number', 'block', 'tread', 'residents', 'occupation', 'exit_date']
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user', None)
+        super().__init__(*args, **kwargs)
+
+# Definindo o formulário de visitante
+class VisitorForm(forms.ModelForm):
+    class Meta:
+        model = Visitor
+        fields = ['name', 'document', 'telephone',]
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user', None)
+        super().__init__(*args, **kwargs)
+
+class FinanceForm(forms.ModelForm):
+    class Meta:
+        model = Finance
+        fields = ['value', 'description', 'document']
+
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user', None)
+        super().__init__(*args, **kwargs)
+
+class VehicleForm(forms.ModelForm):
+    class Meta:
+        model = Vehicle
+        fields = ['plate', 'model', 'color', 'garage', 'owner']
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user', None)
+        super().__init__(*args, **kwargs)
+
+
+class OrdersForm(forms.ModelForm):
+    class Meta:
+        model = Orders
+        fields = ['order_code','status', 'owner', 'signature_image']
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user', None)
+        super().__init__(*args, **kwargs)
